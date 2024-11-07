7fc0ebb3e28d273fc85a0a2161bc9622a98e7be3
Darri Eythorsson <dae5@hi.is>
2024-11-07 16:25:30 -0700

    documenting code

--- run_CONFLUENCE_batch.sh
+++ run_CONFLUENCE_batch.sh
@@ -6,19 +6,10 @@
 #SBATCH --mem-per-cpu=5G
 
 # Load necessary modules (adjust as needed for your HPC environment)
-. /work/comphyd_lab/local/modules/spack/2024v4/lmod-init-bash
-module unuse $MODULEPATH
-module use /work/comphyd_lab/local/modules/spack/2024v4/modules/linux-rocky8-x86_64/Core/
-
-module load netcdf-fortran/4.6.1
-module load openblas/0.3.27
-module load hdf5/1.14.3
-module load gdal/3.9.2
-module load netlib-lapack/3.11.0
-module load openmpi/4.1.6
-module load python/3.11.7
+module restore confluence_modules
 
 # Activate your Python environment if necessary
-source /home/darri.eythorsson/code/scienv/bin/activate
+source /home/darri/code/confluence_env/bin/activate
 
 # Your commands here
+python CONFLUENCE.py
