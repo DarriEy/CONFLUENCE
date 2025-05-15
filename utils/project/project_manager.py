a0c84b3515e8acf9d672bddfd6bd2fba78b59c48
DarriEy <dae5@hi.is>
2025-05-15 17:06:42 -0600

    fixing wait logic for datatool execution

--- utils/project/project_manager.py
+++ utils/project/project_manager.py
@@ -87,8 +87,8 @@ class ProjectManager:
                 (main_path / subdir).mkdir(parents=True, exist_ok=True)
         
         # If in point mode, update bounding box coordinates
-        if self.config.get('SPATIAL_MODE') == 'Point':
-            self._update_bounding_box_for_point_mode()
+        #if self.config.get('SPATIAL_MODE') == 'Point':
+        #    self._update_bounding_box_for_point_mode()
         
         self.logger.info(f"Project directory created at: {self.project_dir}")
         return self.project_dir
