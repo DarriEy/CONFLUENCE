16bde43c60bc049d458f937f40824fa4e7877d78
DarriEy <dae5@hi.is>
2025-10-16 12:58:49 -0600

    working on external tool comiplation scripts

--- utils/cli/cli_argument_manager.py
+++ utils/cli/cli_argument_manager.py
@@ -451,13 +451,13 @@ cat > Makefile << 'MAKEFILE_END'
 F_MASTER = FUSE_INSTALL_DIR_PLACEHOLDER
 
 # Core directory that contains FUSE source code
-F_KORE_DIR = $(F_MASTER)build/FUSE_SRC/
+F_KORE_DIR = $$(F_MASTER)build/FUSE_SRC/
 
 # Location of the compiled modules
-MOD_PATH = $(F_MASTER)build/
+MOD_PATH = $$(F_MASTER)build/
 
 # Define the directory for the executables
-EXE_PATH = $(F_MASTER)bin/
+EXE_PATH = $$(F_MASTER)bin/
 
 #========================================================================
 # PART 1: Define the libraries and compiler
@@ -470,16 +470,12 @@ NCDFF_LIB_PATH = NCDFF_PATH_PLACEHOLDER
 NCDF_LIB_PATH = NCDF_PATH_PLACEHOLDER
 HDF_LIB_PATH = HDF5_PATH_PLACEHOLDER
 
-LIBRARIES = -L$(NCDFF_LIB_PATH)/lib -lnetcdff -L$(NCDF_LIB_PATH)/lib -lnetcdf -L$(HDF_LIB_PATH)/lib -lhdf5_hl -lhdf5
-INCLUDE = -I$(NCDFF_LIB_PATH)/include -I$(NCDF_LIB_PATH)/include -I$(HDF_LIB_PATH)/include
+LIBRARIES = -L$$(NCDFF_LIB_PATH)/lib -lnetcdff -L$$(NCDF_LIB_PATH)/lib -lnetcdf -L$$(HDF_LIB_PATH)/lib -lhdf5_hl -lhdf5
+INCLUDE = -I$$(NCDFF_LIB_PATH)/include -I$$(NCDF_LIB_PATH)/include -I$$(HDF_LIB_PATH)/include
 
 # Define the driver program
-FUSE_DRIVER = \
-  sobol.f90 \
-  fuse_metric.f90 \
-  functn.f90 \
-  fuse_driver.f90
-DRIVER = $(patsubst %, $(DRIVER_DIR)/%, $(FUSE_DRIVER))
+FUSE_DRIVER = sobol.f90 fuse_metric.f90 functn.f90 fuse_driver.f90
+DRIVER = $$(patsubst %, $$(DRIVER_DIR)/%, $$(FUSE_DRIVER))
 
 # Define the executable
 DRIVER_EX = fuse.exe
@@ -489,153 +485,58 @@ DRIVER_EX = fuse.exe
 #========================================================================
 
 # Define directories
-NUMREC_DIR = $(F_KORE_DIR)FUSE_NR
-HOOKUP_DIR = $(F_KORE_DIR)FUSE_HOOK
-DRIVER_DIR = $(F_KORE_DIR)FUSE_DMSL
-NETCDF_DIR = $(F_KORE_DIR)FUSE_NETCDF
-ENGINE_DIR = $(F_KORE_DIR)FUSE_ENGINE
-SCE_DIR    = $(F_KORE_DIR)FUSE_SCE
-TIME_DIR   = $(F_KORE_DIR)FUSE_TIME
+NUMREC_DIR = $$(F_KORE_DIR)FUSE_NR
+HOOKUP_DIR = $$(F_KORE_DIR)FUSE_HOOK
+DRIVER_DIR = $$(F_KORE_DIR)FUSE_DMSL
+NETCDF_DIR = $$(F_KORE_DIR)FUSE_NETCDF
+ENGINE_DIR = $$(F_KORE_DIR)FUSE_ENGINE
+SCE_DIR    = $$(F_KORE_DIR)FUSE_SCE
+TIME_DIR   = $$(F_KORE_DIR)FUSE_TIME
 
 # Utility modules
-FUSE_UTILMS= \
-	kinds_dmsl_kit_FUSE.f90 \
-	utilities_dmsl_kit_FUSE.f90 \
-	fuse_fileManager.f90
-UTILMS = $(patsubst %, $(HOOKUP_DIR)/%, $(FUSE_UTILMS))
+FUSE_UTILMS = kinds_dmsl_kit_FUSE.f90 utilities_dmsl_kit_FUSE.f90 fuse_fileManager.f90
+UTILMS = $$(patsubst %, $$(HOOKUP_DIR)/%, $$(FUSE_UTILMS))
 
 # Numerical Recipes utilities
-FUSE_NRUTIL= \
-	nrtype.f90 \
-	nr.f90 nrutil.f90
-NRUTIL = $(patsubst %, $(NUMREC_DIR)/%, $(FUSE_NRUTIL))
+FUSE_NRUTIL = nrtype.f90 nr.f90 nrutil.f90
+NRUTIL = $$(patsubst %, $$(NUMREC_DIR)/%, $$(FUSE_NRUTIL))
 
 # Data modules
-FUSE_DATAMS= \
-  model_defn.f90 \
-	model_defnames.f90 \
-	multiconst.f90 \
-	multiforce.f90 \
-	multibands.f90 \
-	multiparam.f90 \
-	multistate.f90 \
-	multi_flux.f90 \
-	multiroute.f90 \
-	multistats.f90 \
-	model_numerix.f90
-DATAMS = $(patsubst %, $(ENGINE_DIR)/%, $(FUSE_DATAMS))
+FUSE_DATAMS = model_defn.f90 model_defnames.f90 multiconst.f90 multiforce.f90 multibands.f90 multiparam.f90 multistate.f90 multi_flux.f90 multiroute.f90 multistats.f90 model_numerix.f90
+DATAMS = $$(patsubst %, $$(ENGINE_DIR)/%, $$(FUSE_DATAMS))
 
 # Time I/O modules
-FUSE_TIMEMS= \
-  time_io.f90
-TIMUTILS = $(patsubst %, $(TIME_DIR)/%, $(FUSE_TIMEMS))
+FUSE_TIMEMS = time_io.f90
+TIMUTILS = $$(patsubst %, $$(TIME_DIR)/%, $$(FUSE_TIMEMS))
 
 # Information modules
-FUSE_INFOMS= \
-	metaoutput.f90 \
-	metaparams.f90 \
-	meta_stats.f90 \
-	selectmodl.f90 \
-	putpar_str.f90 \
-	getpar_str.f90 \
-	par_insert.f90 \
-	parextract.f90 \
-	varextract.f90 \
-	sumextract.f90 \
-	str_2_xtry.f90 \
-	xtry_2_str.f90
-INFOMS = $(patsubst %, $(ENGINE_DIR)/%, $(FUSE_INFOMS))
+FUSE_INFOMS = metaoutput.f90 metaparams.f90 meta_stats.f90 selectmodl.f90 putpar_str.f90 getpar_str.f90 par_insert.f90 parextract.f90 varextract.f90 sumextract.f90 str_2_xtry.f90 xtry_2_str.f90
+INFOMS = $$(patsubst %, $$(ENGINE_DIR)/%, $$(FUSE_INFOMS))
 
 # Numerical Recipes
-FUSE_NR_SUB= \
-	ludcmp.f90 lubksb.f90 svbksb.f90 svdcmp.f90 pythag.f90 \
-	gammln.f90 gammp.f90 gcf.f90 gser.f90
-NR_SUB = $(patsubst %, $(NUMREC_DIR)/%, $(FUSE_NR_SUB))
+FUSE_NR_SUB = ludcmp.f90 lubksb.f90 svbksb.f90 svdcmp.f90 pythag.f90 gammln.f90 gammp.f90 gcf.f90 gser.f90
+NR_SUB = $$(patsubst %, $$(NUMREC_DIR)/%, $$(FUSE_NR_SUB))
 
 # Model guts
-FUSE_MODGUT=\
-	mod_derivs.f90 \
-	update_swe.f90 \
-	qrainerror.f90 \
-	qsatexcess.f90 \
-	evap_upper.f90 \
-	evap_lower.f90 \
-	qinterflow.f90 \
-	qpercolate.f90 \
-	q_baseflow.f90 \
-	q_misscell.f90 \
-	logismooth.f90 \
-	mstate_eqn.f90 \
-	fix_states.f90 \
-	meanfluxes.f90 \
-	wgt_fluxes.f90 \
-	updatstate.f90 \
-	q_overland.f90
-MODGUT = $(patsubst %, $(ENGINE_DIR)/%, $(FUSE_MODGUT))
+FUSE_MODGUT = mod_derivs.f90 update_swe.f90 qrainerror.f90 qsatexcess.f90 evap_upper.f90 evap_lower.f90 qinterflow.f90 qpercolate.f90 q_baseflow.f90 q_misscell.f90 logismooth.f90 mstate_eqn.f90 fix_states.f90 meanfluxes.f90 wgt_fluxes.f90 updatstate.f90 q_overland.f90
+MODGUT = $$(patsubst %, $$(ENGINE_DIR)/%, $$(FUSE_MODGUT))
 
 # Solver
-FUSE_SOLVER= \
-	interfaceb.f90 \
-	limit_xtry.f90 \
-	viol_state.f90 \
-	fuse_deriv.f90 \
-	fmin.f90 fdjac_ode.f90 flux_deriv.f90 disaggflux.f90 \
-	fuse_sieul.f90 \
-	newtoniter.f90 lnsrch.f90
-SOLVER = $(patsubst %, $(ENGINE_DIR)/%, $(FUSE_SOLVER))
+FUSE_SOLVER = interfaceb.f90 limit_xtry.f90 viol_state.f90 fuse_deriv.f90 fmin.f90 fdjac_ode.f90 flux_deriv.f90 disaggflux.f90 fuse_sieul.f90 newtoniter.f90 lnsrch.f90
+SOLVER = $$(patsubst %, $$(ENGINE_DIR)/%, $$(FUSE_SOLVER))
 
 # Define routines for FUSE preliminaries
-FUSE_PRELIM= \
-	ascii_util.f90 \
-	uniquemodl.f90 \
-	getnumerix.f90 \
-	getparmeta.f90 \
-	assign_stt.f90 \
-	assign_flx.f90 \
-	assign_par.f90 \
-	adjust_stt.f90 \
-	par_derive.f90 \
-	bucketsize.f90 \
-	mean_tipow.f90 \
-	qbsaturatn.f90 \
-	qtimedelay.f90 \
-	init_stats.f90 \
-	init_state.f90
-PRELIM = $(patsubst %, $(ENGINE_DIR)/%, $(FUSE_PRELIM))
+FUSE_PRELIM = ascii_util.f90 uniquemodl.f90 getnumerix.f90 getparmeta.f90 assign_stt.f90 assign_flx.f90 assign_par.f90 adjust_stt.f90 par_derive.f90 bucketsize.f90 mean_tipow.f90 qbsaturatn.f90 qtimedelay.f90 init_stats.f90 init_state.f90
+PRELIM = $$(patsubst %, $$(ENGINE_DIR)/%, $$(FUSE_PRELIM))
 
-FUSE_MODRUN= \
-        metrics.f90 \
-	conv_funcs.f90 \
-	force_info.f90 \
-	clrsky_rad.f90 \
-	getPETgrid.f90 \
-	get_mbands.f90 \
-	get_time_indices.f90\
-	initfluxes.f90 \
-	set_all.f90 \
-	ode_int.f90 \
-	fuse_solve.f90 \
-	comp_stats.f90 \
-	mean_stats.f90
-MODRUN = $(patsubst %, $(ENGINE_DIR)/%, $(FUSE_MODRUN))
+FUSE_MODRUN = metrics.f90 conv_funcs.f90 force_info.f90 clrsky_rad.f90 getPETgrid.f90 get_mbands.f90 get_time_indices.f90 initfluxes.f90 set_all.f90 ode_int.f90 fuse_solve.f90 comp_stats.f90 mean_stats.f90
+MODRUN = $$(patsubst %, $$(ENGINE_DIR)/%, $$(FUSE_MODRUN))
 
 # Define NetCDF routines
-FUSE_NETCDF = \
-    handle_err.f90 \
-    extractor.f90 juldayss.f90 caldatss.f90 \
-    get_gforce.f90 \
-    get_smodel.f90 \
-    get_fparam.f90 \
-    def_params.f90 \
-    def_output.f90 \
-    def_sstats.f90 \
-    put_params.f90 \
-    put_output.f90 \
-    put_sstats.f90
-NETCDF = $(patsubst %, $(NETCDF_DIR)/%, $(FUSE_NETCDF))
+FUSE_NETCDF = handle_err.f90 extractor.f90 juldayss.f90 caldatss.f90 get_gforce.f90 get_smodel.f90 get_fparam.f90 def_params.f90 def_output.f90 def_sstats.f90 put_params.f90 put_output.f90 put_sstats.f90
+NETCDF = $$(patsubst %, $$(NETCDF_DIR)/%, $$(FUSE_NETCDF))
 
-SCE = \
-	sce_16plus.o
+SCE = sce_16plus.o
 
 #========================================================================
 # PART 3: Compile
@@ -644,21 +545,15 @@ SCE = \
 FLAGS = -O3 -ffree-line-length-none -fmax-errors=0 -cpp -fallow-argument-mismatch
 
 # Compile SCE code written in Fortran 77
-sce_16plus.o: $(SCE_DIR)/sce_16plus.f
-	$(FC) -O2 -c -ffixed-form -o $@ $
+sce_16plus.o: $$(SCE_DIR)/sce_16plus.f
+	$$(FC) -O2 -c -ffixed-form -o $$@ $$
 
 # Compile
 all: compile install clean
 
 # compile FUSE
 compile: sce_16plus.o
-	$(FC) $(FLAGS) $(INCLUDE) \
-        sce_16plus.o \
-        $(UTILMS) $(NRUTIL) $(DATAMS) $(TIMUTILS) $(INFOMS) \
-        $(NR_SUB) $(MODGUT) $(SOLVER) $(PRELIM) $(MODRUN) \
-        $(NETCDF) $(DRIVER) \
-        $(LIBRARIES) \
-        -o $(DRIVER_EX)
+	$$(FC) $$(FLAGS) $$(INCLUDE) sce_16plus.o $$(UTILMS) $$(NRUTIL) $$(DATAMS) $$(TIMUTILS) $$(INFOMS) $$(NR_SUB) $$(MODGUT) $$(SOLVER) $$(PRELIM) $$(MODRUN) $$(NETCDF) $$(DRIVER) $$(LIBRARIES) -o $$(DRIVER_EX)
 		
 # Remove object files
 clean:
@@ -668,9 +563,9 @@ clean:
 
 # Copy the executable to the bin directory
 install:
-	mkdir -p $(EXE_PATH)
-	mv $(DRIVER_EX) $(EXE_PATH)
-	@echo "✅ FUSE executable installed to: $(EXE_PATH)"
+	mkdir -p $$(EXE_PATH)
+	mv $$(DRIVER_EX) $$(EXE_PATH)
+	@echo "✅ FUSE executable installed to: $$(EXE_PATH)"
 MAKEFILE_END
 
 # Replace placeholders with actual paths
